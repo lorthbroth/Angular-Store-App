5fec079e650784e2eb3d03d208569eb451c2d760
Dominic Kgoete (ext.) <dominic.kgoete@bmwithub.co.za>
2023-10-09 09:10:13 +0200

    Adding getAll function into the BeerService that gives us all the food images as an array

--- Store-app/src/app/services/beer/beer.service.ts
+++ Store-app/src/app/services/beer/beer.service.ts
@@ -6,4 +6,15 @@ import { Injectable } from '@angular/core';
 export class BeerService {
 
   constructor() { }
+
+  getAll():String[]{
+    return [
+      '/Store-app/src/assets/images/beers/c-2.jpg',
+      '/Store-app/src/assets/images/beers/h-1.jpg',
+      '/Store-app/src/assets/images/beers/b-3.jpg',
+      '/Store-app/src/assets/images/beers/s-4.jpg',
+      '/Store-app/src/assets/images/beers/w-5.jpg',
+      '/Store-app/src/assets/images/beers/a-6.jpg'
+    ]
+  }
 }
