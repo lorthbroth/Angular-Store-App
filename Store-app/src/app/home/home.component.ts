8fbb4dee11abc0fdb56a114a578d64e22af16660
Dominic Kgoete (ext.) <dominic.kgoete@bmwithub.co.za>
2023-10-25 14:22:45 +0200

    Injecting the beer service into the home component

--- Store-app/src/app/home/home.component.ts
+++ Store-app/src/app/home/home.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { BeerService } from '../services/beer/beer.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  beers:String[] = [];
+  constructor(private beerService:BeerService) {}
+
+  ngOnInit(): void{
+    this.beers = this.beerService.getAll();
+  }
 
 }
